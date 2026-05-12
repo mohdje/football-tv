31137a219355d5ac79e30e640dfa63080bb5f3ac
mohdje <djellebm@hotmail.fr>
2026-05-12 11:54:14 +0100

    improve SEO

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
-
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
 );
+
 
 
